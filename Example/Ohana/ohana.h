1e31795d7a9ec3ee7a8b5b742db6e598723cb04f
xxxoo <xxxoo>
2020-10-14 21:58:01 -0700

    fix build issue

---
+++ Example/Ohana/ohana.h
@@ -0,0 +1,24 @@
+//
+//  ohana.h
+//  ohana
+//
+//  Created by xuwenquan on 2020/10/13.
+//  Copyright © 2020 Nick Entin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ohana.
+FOUNDATION_EXPORT double ohanaVersionNumber;
+
+//! Project version string for ohana.
+FOUNDATION_EXPORT const unsigned char ohanaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ohana/PublicHeader.h>
+
+
+
+#import <ohana/OhanaCore.h>
+#import <ohana/OhanaCommon.h>
+#import <ohana/OhanaUtilities.h>
+//#import <ohana/OHContactsDataProviderProtocol>
