37ecebf72255d69b83fcaa87c0d75c0a58ac90e7
xxxoo <xxxoo>
2020-12-15 22:08:02 -0800

    could publish

---
+++ Example/libohana/libohana.h
@@ -0,0 +1,13 @@
+//
+//  libohana.h
+//  libohana
+//
+//  Created by xuwenquan on 2020/12/12.
+//  Copyright © 2020 Nick Entin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface libohana : NSObject
+
+@end
---
+++ Example/libohana/libohana.m
@@ -0,0 +1,13 @@
+//
+//  libohana.m
+//  libohana
+//
+//  Created by xuwenquan on 2020/12/12.
+//  Copyright © 2020 Nick Entin. All rights reserved.
+//
+
+#import "libohana.h"
+
+@implementation libohana
+
+@end
